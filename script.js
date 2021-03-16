1cefdfc56959bb12ea22ab9bc53d288d58717d23
ashutoshmadhwani <58286085+ashutoshmadhwani@users.noreply.github.com>
2021-03-16 18:45:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const bcrypt=require('bcrypt-nodejs');
+const cors=require('cors');
+const { response } = require('express');
+const db=require('knex')({
+    client: 'pg',
+    connection:{
+        host:'127.0.0.1',
+        user:'postgres',
+        password:'root',
+        database:'smartbrain',
+    }
+});
+const register =require('./Controllers/register');
+const signin =require('./Controllers/signin');
+const profile =require('./Controllers/profile');
+const image =require('./Controllers/image');
+
+
+const app=express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{res.send(database.user);})
+app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
+app.post("/register",(req,res) =>{register.handleRegister(req,res,db,bcrypt)})
+app.get('/profile/:id',(req,res)=>{profile.handleProfileGet(req,res,db)})
+app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
+app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
+
+
+app.listen(3000,()=>{
+    console.log('app is running');
+})
